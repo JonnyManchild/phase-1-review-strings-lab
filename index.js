0b976ddc64ab3c8d392b1c22357334bc2c82db4a
Jonathan MacDuff <jonathan.macduff@outlook.com>
2023-12-11 12:07:51 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Jonathan MacDuff";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
